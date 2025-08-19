383e3ba44549c2bfba3e4ebf167388bda664315a
Till Schallau <till.schallau@tu-dortmund.de>
2025-08-20 00:24:42 +0200

    Remove Town04 from allowed maps, as some features are not yet supported
    Signed-off-by: Till Schallau <till.schallau@tu-dortmund.de>

--- carla_interaction_gui/unified_gui.py
+++ carla_interaction_gui/unified_gui.py
@@ -18,7 +18,12 @@ from carla_interaction_gui.workers.TransformRecordingWorker import TransformReco
 
 # Official CARLA maps from the docs (non-layered + layered "_Opt")
 ALLOWED_NON_LAYERED_MAPS = [
-    "Town01", "Town02", "Town03", "Town04", "Town05", "Town10HD_Opt",
+    "Town01",
+    "Town02",
+    "Town03",
+    # "Town04",
+    "Town05",
+    "Town10HD_Opt",
 ]
 ALLOWED_CARLA_MAPS = ALLOWED_NON_LAYERED_MAPS
 
